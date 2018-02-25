5a2f90543f9519af3fcad133e8db84ab31203b96
Lindsay Folkard <lindsayfolakrd@gmail.com>
2018-02-25 17:44:06 +1100

    WIP

--- node/l6470_driver/test/stepper_motor_test.cpp
+++ node/l6470_driver/test/stepper_motor_test.cpp
@@ -80,7 +80,7 @@ int main (int argc, char ** argv)
     //    GoToCommand goToCommand(10000);
     //    cout << "Pos, converted is " << (int)goToCommand.pos;
     //    driver.goTo(goToCommand,0);
-    RunCommand runCommand(Reverse,700);
+    RunCommand runCommand(Forward,700);
     driver.run(runCommand,0);
     cout << "Run Command is : " << runCommand << std::endl;
 
